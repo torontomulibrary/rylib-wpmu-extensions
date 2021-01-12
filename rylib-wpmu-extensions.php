cde46a6f531e42184d495802aac19cb7b109833f
Patrick Fung <patrick@makestuffdostuff.com>
2021-01-12 12:15:23 -0500

    version bump

--- rylib-wpmu-extensions.php
+++ rylib-wpmu-extensions.php
@@ -7,7 +7,7 @@ defined( 'ABSPATH' ) OR exit;
  * Author URI: https://github.com/ryersonlibrary
  * Description: Extra functionality for WordPress Multisite for the Ryerson University Library WordPress site.
  * GitHub Plugin URI: https://github.com/ryersonlibrary/rylib-wpmu-extensions
- * Version: 0.0.1-alpha
+ * Version: 0.0.2-alpha
  */
 
 // Adds "Active Theme" column to sites listing
