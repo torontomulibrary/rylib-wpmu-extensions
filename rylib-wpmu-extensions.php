b940c728f5f63c95736ac34d59b155c38e46dcb0
Patrick Fung <patrick@makestuffdostuff.com>
2021-01-12 12:20:05 -0500

    add site id coumn

--- rylib-wpmu-extensions.php
+++ rylib-wpmu-extensions.php
@@ -7,13 +7,16 @@ defined( 'ABSPATH' ) OR exit;
  * Author URI: https://github.com/ryersonlibrary
  * Description: Extra functionality for WordPress Multisite for the Ryerson University Library WordPress site.
  * GitHub Plugin URI: https://github.com/ryersonlibrary/rylib-wpmu-extensions
- * Version: 0.0.2-alpha
+ * Version: 0.0.3-alpha
  */
 
-// Adds "Active Theme" column to sites listing
-function rylib_wpmu_blogs_columns_active_theme($sites_columns)
+// Adds custom columns to Sites list in Network Options
+function rylib_wpmu_blogs_columns($sites_columns)
 {
-  $sites_columns = $sites_columns + array( 'template' => 'Active Theme' );
+  $sites_columns = $sites_columns + array( 
+    'blog_id' => 'Site ID',
+    'template' => 'Active Theme' 
+  );
   return $sites_columns;
 }
 add_filter( 'wpmu_blogs_columns', 'rylib_wpmu_blogs_columns_active_theme' );
@@ -21,6 +24,10 @@ add_filter( 'wpmu_blogs_columns', 'rylib_wpmu_blogs_columns_active_theme' );
 // Hook to manage column data on network sites listing
 function rylib_wpmu_manage_sites_custom_column_active_theme( $column_name, $blog_id )
 {
+  if ( $column_name == 'blog_id' ) {
+    echo "{$blog_id}";
+  }
+
   if ( $column_name == 'template' ) {
     $template_directory = get_blog_option( $blog_id, 'template' );
     $theme = wp_get_theme( $template_directory );
